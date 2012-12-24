06a550cb5bbdb26998c187f7e3093bf13c7ca82e
Arjan Scherpenisse <arjan@scherpenisse.net>
2012-12-24 10:32:40 +0100

    Tweaks to reset script

--- reset/reset.sh
+++ reset/reset.sh
@@ -10,11 +10,11 @@ PSQL="sudo -u postgres psql zotonicdemo"
 
 /etc/init.d/zotonic stop
 
-/usr/bin/sudo -u zotonic -i ./update.sh
+/usr/bin/sudo -u zotonic -i $DEMO/reset/update.sh
 
 echo "DROP SCHEMA public CASCADE" | $PSQL
 echo "CREATE SCHEMA public AUTHORIZATION zotonic" | $PSQL
-$PSQL < /home/zotonic/reset/demo.sql
+$PSQL < $DEMO/reset/demo.sql
 
 rm -rf $DEMO/files/*/*
 
